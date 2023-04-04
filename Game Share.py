96ec283082ff3cf85f67bd0e39727ebf18004377
VViikkttoorr <99556576+VViikkttoorr@users.noreply.github.com>
2023-04-04 14:42:45 +0600

    Add files via upload

--- Game Share.py
+++ Game Share.py
@@ -1,86 +1,80 @@
 import tkinter as tk
 import random
 
-class BallGame:
-    def __init__(self, master):
-        self.master = master
-        master.title("Ball Game")
-        master.geometry("400x450")
+# Создаем окно
+window = tk.Tk()
+window.title("Игра с шарами")
 
-        self.canvas = tk.Canvas(master, width=300, height=300, bg="white")
-        self.canvas.pack(pady=20)
+# Устанавливаем размер окна
+window.geometry("500x500")
 
-        self.score_label = tk.Label(master, text="Score: 0")
-        self.score_label.pack()
+# Создаем метку для отображения счета
+score_label = tk.Label(window, text="Счет: 0")
+score_label.pack()
 
-        self.missed_label = tk.Label(master, text="Missed: 0")
-        self.missed_label.pack()
+# Создаем метку для отображения таймера
+timer_label = tk.Label(window, text="Время: 10")
+timer_label.pack()
 
-        self.start_button = tk.Button(master, text="Start", command=self.start_game)
-        self.start_button.pack()
+# Создаем кнопку для сброса счета
+def reset_score():
+    global red_count, green_count, blue_count
+    red_count, green_count, blue_count = 0, 0, 0
+    score_label.config(text="Счет: 0 (красных: 0, зеленых: 0, синих: 0)")
 
-        self.timer_label = tk.Label(master, text="")
-        self.timer_label.pack()
+reset_button = tk.Button(window, text="Сбросить счет", command=reset_score)
+reset_button.pack()
 
-        self.canvas.bind("<Button-1>", self.handle_click)
+# Создаем холст для отображения шаров
+canvas = tk.Canvas(window, width=400, height=400)
+canvas.pack()
 
-        self.balls = []
-        self.colors = ["blue", "red", "yellow"]
-        self.sizes = [6, 9, 12]
-        self.score = {"blue": 0, "red": 0, "yellow": 0}
-        self.missed = 0
-        self.timer = None
-        self.time_left = 10
+# Определяем цвета шаров
+colors = ["red", "green", "blue"]
 
-    def start_game(self):
-        self.start_button.config(state=tk.DISABLED)
-        self.spawn_ball()
-        self.timer_label.config(text=f"Time left: {self.time_left}")
-        self.timer = self.master.after(1000, self.update_timer)
+# Создаем функцию для создания шаров
+def create_ball():
+    # Генерируем случайные координаты для шара
+    x = random.randint(50, 350)
+    y = random.randint(50, 350)
+    
+    # Выбираем случайный цвет для шара
+    color = random.choice(colors)
+    
+    # Создаем шар на холсте
+    ball = canvas.create_oval(x, y, x+50, y+50, fill=color)
+    
+    # Увеличиваем счет при попадании на шар
+    def on_click(event):
+        global red_count, green_count, blue_count
+        if color == "red":
+            red_count += 1
+        elif color == "green":
+            green_count += 1
+        elif color == "blue":
+            blue_count += 1
+        score_label.config(text=f"Счет: {red_count+green_count+blue_count} (красных: {red_count}, зеленых: {green_count}, синих: {blue_count})")
+        canvas.delete(ball)
+        create_ball()
+        
+    canvas.tag_bind(ball, "<Button-1>", on_click)
 
-    def spawn_ball(self):
-        color = random.choice(self.colors)
-        size = random.choice(self.sizes)
-        x = random.randint(size, 300 - size)
-        y = random.randint(size, 300 - size)
-        ball = self.canvas.create_oval(x - size, y - size, x + size, y + size, fill=color)
-        self.balls.append(ball)
-        self.master.after(1000, self.spawn_ball)
+# Создаем функцию для старта игры
+def start_game():
+    global red_count, green_count, blue_count
+    red_count, green_count, blue_count = 0, 0, 0
+    score_label.config(text="Счет: 0 (красных: 0, зеленых: 0, синих: 0)")
+    create_ball()
+    window.after(10000, end_game)
 
-    def update_timer(self):
-        self.time_left -= 1
-        self.timer_label.config(text=f"Time left: {self.time_left}")
-        if self.time_left == 0:
-            self.end_game()
-        else:
-            self.timer = self.master.after(1000, self.update_timer)
+# Создаем функцию для окончания игры
+def end_game():
+    canvas.delete("all")
+    timer_label.config(text="Время: 0")
 
-    def handle_click(self, event):
-        x, y = event.x, event.y
-        for ball in self.balls:
-            coords = self.canvas.coords(ball)
-            if coords[0] <= x <= coords[2] and coords[1] <= y <= coords[3]:
-                color = self.canvas.itemcget(ball, "fill")
-                self.score[color] += 1
-                self.score_label.config(text=f"Score: {sum(self.score.values())}")
-                self.canvas.delete(ball)
-                self.balls.remove(ball)
-                return
-        self.missed += 1
-        self.missed_label.config(text=f"Missed: {self.missed}")
+# Создаем кнопку для начала игры
+start_button = tk.Button(window, text="Начать игру", command=start_game)
+start_button.pack()
 
-    def end_game(self):
-        self.master.after_cancel(self.timer)
-        self.canvas.delete("all")
-        self.balls.clear()
-        self.score = {"blue": 0, "red": 0, "yellow": 0}
-        self.missed = 0
-        self.time_left = 10
-        self.score_label.config(text="Score: 0")
-        self.missed_label.config(text="Missed: 0")
-        self.timer_label.config(text="")
-        self.start_button.config(state=tk.NORMAL)
-
-root = tk.Tk()
-game = BallGame(root)
-root.mainloop()
+# Запускаем главный цикл
+window.mainloop()
